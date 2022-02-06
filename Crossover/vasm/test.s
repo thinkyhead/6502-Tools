1f70ffe1b1b4ffe9b918460a13b6496fcb4a16f4
Scott Lahteine <github@thinkyhead.com>
2022-02-06 08:46:27 -0600

    vasm test code

---
+++ Crossover/vasm/test.s
@@ -0,0 +1,27 @@
+;
+; test.s - 6502 Assembler Test Code
+;
+	.org $0602
+start
+	lda #<data
+	sta $00
+	lda #>data
+	sta $01
+
+	lda #0
+	ldx #25
+
+loop	adc #2
+	dex
+	bne loop
+
+	rts
+
+data	.word start
+
+	.word $0201
+	.word $0302
+	.word $0403
+	.word $0504
+	.word $0605
+	.word $0706
